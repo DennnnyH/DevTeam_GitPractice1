31c7d7b6b68750e8014c575cb7af99f47b07e3fe
DennnnyH <97718766+DennnnyH@users.noreply.github.com>
2022-01-13 19:01:05 -1000

    A2 and A4

--- PracticeOne.java
+++ PracticeOne.java
@@ -3,31 +3,21 @@ public class PracticeOne {
         System.out.println("Hello World");
 
         // A1: print "MOHS"
-
         // A2: multiply 4 times 4 and print the result
-
-
-
-            // A3: create an array named "arr" that contains 3, 5, 1, 6, 3, 3
-
+        System.out.println(4*4);
+        // A3: create an array named "arr" that contains 3, 5, 1, 6, 3, 3
         // A4: create a 2D array named "array2D": { {1, 2, 3}, {4, 5, 6} }
+        int[][] array2D = new int [2][2];
 
     }
 
     // M1: write a method on the occurences of the number 3 in the array
     // (and call it in the main method)
-
     // M2: write a method that removes the last element of an array
     // (and call it in the main method)
-
     // M3: write a method that adds the sum of all numbers in a array
     // (and call it in the main method)
-
     // M4: write a sorting algorithm (and call it in the main method)
-    public static void multiply(int total){
-        total = 4 * 4;
-        System.out.println(total);
-    }
-    
+
 }
 
